de4b82e3245de3a67714dde92de78d8786624c7e
Atin Arora <heyohjimbo@gmail.com>
2022-08-27 00:54:05 +0530

    template basics

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,30 @@
+from multiprocessing import context
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+
+posts = [
+	{
+		'author' : 'Atin Arora',
+		'title' : 'Post 1',
+		'content' : 'Mera balam thanedar',
+		'date_posted' : '26/8/2022'
+	},
+	{
+		'author' : 'Nadu Man',
+		'title' : 'Post 69',
+		'content' : 'I love sutta',
+		'date_posted' : '27/8/2022'
+	},
+]	
+	
 
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context = {
+		'title' : 'Home',
+		'posts' : posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+#	return HttpResponse('<h1>Blog About</h1>')
+	return render(request, 'blog/about.html')
