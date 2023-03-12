e545469a8c5ff31529fbd6218c19e631fa6b6c47
Atin Arora <heyohjimbo@gmail.com>
2023-03-12 16:42:17 +0530

    check account

--- djproj/urls.py
+++ djproj/urls.py
@@ -13,16 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from tempfile import template
 from django.contrib import admin
 from django.urls import path, include
 from users import views as users_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('', include('blog.urls')),
     path('register/', users_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('', include('blog.urls')),
 
-
-#    path('', include('blog.urls')),
 ]
