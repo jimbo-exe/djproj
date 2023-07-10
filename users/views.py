1ea525ae4b7326e43cb2eb0cd05b213d7d6dd88c
Atin Arora <heyohjimbo@gmail.com>
2023-07-11 03:45:09 +0530

    add profile page

--- users/views.py
+++ users/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render, redirect
 #from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
-messages
+'''
+messages.success
+messages.warning
+messages.info
+messages.debug
+messages.error
+'''
 
 def register(request):
   if request.method == 'POST':
@@ -15,5 +22,9 @@ def register(request):
       messages.success(request, f'Account created for {username}!')
       return redirect('login')
   else:  
-   form = UserRegisterForm()
+    form = UserRegisterForm()
   return render(request, 'users/register.html', {'form' : form})
+
+@login_required
+def profile(request):
+  return render(request, 'users/profile.html')
